5a5a69251006784d12b541de4486e19011a3bb14
Arthur Feitosa Rodrigues dos Santos <arthurfeitosar64@gmail.com>
2026-02-26 09:16:54 -0300

    Update desafio_1002.py

--- desafio_1002.py
+++ desafio_1002.py
@@ -18,3 +18,6 @@ exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla p
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1002
 
 # Escreva sua solução abaixo
+R = float(input())
+A = (3.14159 * (R**2))
+print(f"A = {A:.4f}")
