1e7bb2490d94e192a5278fe221eb106369617e48
Arthur Feitosa Rodrigues dos Santos <arthurfeitosar64@gmail.com>
2026-02-26 09:04:56 -0300

    Update desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -12,3 +12,6 @@ você receberá "Presentation Error".
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1001
 
 # Escreva sua solução abaixo
+A,B = map(int, input().split())
+X = A+B
+print(X)
