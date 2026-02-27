dce880b50af1a27c283fa0c4b23584c886f43f3e
Arthur Feitosa Rodrigues dos Santos <arthurfeitosar64@gmail.com>
2026-02-26 22:41:49 -0300

    Update desafio_1005.py

--- desafio_1005.py
+++ desafio_1005.py
@@ -20,4 +20,4 @@ de imprimir o fim de linha após o resultado, caso contrário, você receberá
 # Escreva sua solução abaixo
 A, B = map(float, input().split())
 M = (A * 3.5 + B * 7.5)/11
-print(f"MEDIA = {M}")
+print(f"MEDIA = {M:.1f}")
